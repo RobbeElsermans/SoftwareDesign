f31fe0035507d27b86bf20c3fb1792c6510b9754
Robbe Elsermans <robels@outlook.com>
2022-11-28 17:14:26 +0100

    Add default GUI menu

--- MoneyTracker/src/main/java/GUI/guiController/ControllerMain.java
+++ MoneyTracker/src/main/java/GUI/guiController/ControllerMain.java
@@ -3,12 +3,15 @@ package GUI.guiController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class ControllerMain {
+public class ControllerMain implements Initializable {
 
     @FXML
     private BorderPane mainPane;
@@ -51,6 +54,17 @@ public class ControllerMain {
         Pane view = null;
         try {
             view = FXMLLoader.load(getClass().getResource("/fxml/VariableTicket.fxml"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        mainPane.setCenter(view);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Pane view = null;
+        try {
+            view = FXMLLoader.load(getClass().getResource("/fxml/CreatePerson.fxml"));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
