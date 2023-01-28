14fc142efa7ae2c4748b23ee7c9e9c690859f649
Robbe Elsermans <robels@outlook.com>
2023-01-28 12:45:44 +0100

    Add extra integration tests

--- MoneyTracker/src/test/java/calculator/IntegrationTest_Tallies.java
+++ MoneyTracker/src/test/java/calculator/IntegrationTest_Tallies.java
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class IntegrationTest_Tallies {
 
@@ -122,6 +122,8 @@ public class IntegrationTest_Tallies {
         } else if ((personController.getNameById(tallies.get(1).getValue0()).equalsIgnoreCase("A") &&
                 personController.getNameById(tallies.get(1).getValue1()).equalsIgnoreCase("B"))) {
             assertEquals(tallies.get(1).getValue2(), 10.0);
+        } else {
+            fail("No match found");
         }
 
         //AND 0 -> 1 with 10?
@@ -131,6 +133,8 @@ public class IntegrationTest_Tallies {
         } else if (personController.getNameById(tallies.get(1).getValue0()).equalsIgnoreCase("B") &&
                 personController.getNameById(tallies.get(1).getValue1()).equalsIgnoreCase("C")) {
             assertEquals(tallies.get(1).getValue2(), 30.0);
+        } else {
+            fail("No match found");
         }
 
         tallies = Calculator.CalculateFinalTallies(tallies);
@@ -140,11 +144,13 @@ public class IntegrationTest_Tallies {
 
         //It should be 0 -> 1 with 10.0
         if ((personController.getNameById(tallies.get(0).getValue0()).equalsIgnoreCase("A") &&
-                personController.getNameById(tallies.get(0).getValue1()).equalsIgnoreCase("B"))) {
+                personController.getNameById(tallies.get(0).getValue1()).equalsIgnoreCase("C"))) {
             assertEquals(tallies.get(0).getValue2(), 10.0);
         } else if ((personController.getNameById(tallies.get(1).getValue0()).equalsIgnoreCase("A") &&
-                personController.getNameById(tallies.get(1).getValue1()).equalsIgnoreCase("B"))) {
+                personController.getNameById(tallies.get(1).getValue1()).equalsIgnoreCase("C"))) {
             assertEquals(tallies.get(1).getValue2(), 10.0);
+        } else {
+            fail("No match found");
         }
 
         //It should be 1 -> 2 with 20.0
@@ -154,6 +160,8 @@ public class IntegrationTest_Tallies {
         } else if ((personController.getNameById(tallies.get(1).getValue0()).equalsIgnoreCase("B") &&
                 personController.getNameById(tallies.get(1).getValue1()).equalsIgnoreCase("C"))) {
             assertEquals(tallies.get(1).getValue2(), 20.0);
+        } else {
+            fail("No match found");
         }
     }
 
@@ -211,7 +219,7 @@ public class IntegrationTest_Tallies {
                 personController.getNameById(tallies.get(1).getValue1()).equalsIgnoreCase("B"))) {
             assertEquals(tallies.get(1).getValue2(), 10.0);
         } else {
-            assertEquals(true, false, "No match found");
+            fail("No match found");
         }
 
         //It should be 1 -> 2 with 30.0
@@ -222,7 +230,7 @@ public class IntegrationTest_Tallies {
                 personController.getNameById(tallies.get(1).getValue1()).equalsIgnoreCase("C")) {
             assertEquals(tallies.get(1).getValue2(), 30.0);
         } else {
-            assertEquals(true, false, "No match found");
+            fail("No match found");
         }
 
         tallies = Calculator.CalculateFinalTallies(tallies);
@@ -236,7 +244,7 @@ public class IntegrationTest_Tallies {
                 personController.getNameById(tallies.get(1).getValue1()).equalsIgnoreCase("C"))) {
             assertEquals(tallies.get(1).getValue2(), 20.0);
         } else {
-            assertEquals(true, false, "No match found");
+            fail("No match found");
         }
 
         //It should be 2 -> 0 with 10.0
@@ -247,7 +255,7 @@ public class IntegrationTest_Tallies {
                 personController.getNameById(tallies.get(1).getValue1()).equalsIgnoreCase("C"))) {
             assertEquals(tallies.get(1).getValue2(), 10.0);
         } else {
-            assertEquals(true, false, "No match found");
+            fail("No match found");
         }
 
 
@@ -271,103 +279,17 @@ public class IntegrationTest_Tallies {
                 personController.getNameById(tallies.get(1).getValue1()).equalsIgnoreCase("B"))) {
             assertEquals(tallies.get(1).getValue2(), 10.0);
         } else {
-            assertEquals(true, false, "No match found");
+            fail("No match found");
         }
 
     }
 
     @Test
     public void testTreeTalliesWithRemoval2() {
-        for (int i = 0; i < 1000; i++) {
-            //reset databases
-            TicketDB.getInstance().delAllValue();
-            PersonDB.getInstance().delAllValue();
+        //reset databases
+        TicketDB.getInstance().delAllValue();
+        PersonDB.getInstance().delAllValue();
 
-            //Create 3 persons
-            List<IPerson> personList = createPersons(3);
-
-            //Add to database
-            PersonController personController = new PersonController(PersonDB.getInstance());
-            personList.forEach(personController::addValue);
-
-            //Create tickets
-            List<ITicket> ticketList = new ArrayList<>();
-            ITicketFactory ticketFactory = new DinnerTicketFactory();
-            TicketController ticketController = new TicketController(TicketDB.getInstance());
-
-            //Create depth with taxi and uniform between 0 -> 1 for € 20.0
-            //0 (A) pays €20 for 1 (B)
-            HashMap<Integer, Double> debts = new HashMap<>();
-            debts.put(personController.getIdByName(personList.get(1).toString()), 20.0);
-            ITicket ticket = ticketFactory.getUniformTicket(personController.getIdByName(personList.get(0).toString()), debts);
-            ticketList.add(ticket);
-
-            //Create depth with dinner and uniform between 1 -> 0 for € 20.0
-            //1 (B) pays €20for 0 (A)
-            debts = new HashMap<>();
-            debts.put(personController.getIdByName(personList.get(0).toString()), 20.0);
-            //ticket = ticketFactory.getVariableTicket(personController.getIdByName(personList.get(1).toString()), debts);
-            //ticketList.add(ticket);
-
-            //Create depth with dinner and uniform between 1 -> 2 for € 30.0
-            //1 (B) pays €30 for 2 (C)
-            //debts = new HashMap<>();
-            debts.put(personController.getIdByName(personList.get(2).toString()), 30.0);
-            ticket = ticketFactory.getVariableTicket(personController.getIdByName(personList.get(1).toString()), debts);
-            ticketList.add(ticket);
-
-            //save to the database
-            ticketList.forEach(ticketController::addValue);
-
-            //Calculate the tallies.
-            List<Triplet<Integer, Integer, Double>> tallies = Calculator.CalculateTallyPairs();
-            tallies = Calculator.CalculateFinalTallies(tallies);
-            Calculator.PrintTallies(tallies);
-
-            //It should be 0 -> 1 with 0
-
-            //It should be 2 -> 1 with 30.0
-            if (tallies.size() < 2)
-                if (personController.getNameById(tallies.get(0).getValue0()).equalsIgnoreCase("B") &&
-                        personController.getNameById(tallies.get(0).getValue1()).equalsIgnoreCase("C")) {
-                    assertEquals(tallies.get(0).getValue2(), 30.0);
-                } else {
-                    assertEquals(true, false, "No match found");
-                }
-            else {
-                if (personController.getNameById(tallies.get(0).getValue0()).equalsIgnoreCase("B") &&
-                        personController.getNameById(tallies.get(0).getValue1()).equalsIgnoreCase("C")) {
-                    assertEquals(tallies.get(0).getValue2(), 30.0);
-                } else if (personController.getNameById(tallies.get(1).getValue0()).equalsIgnoreCase("B") &&
-                        personController.getNameById(tallies.get(1).getValue1()).equalsIgnoreCase("C")) {
-                    assertEquals(tallies.get(1).getValue2(), 30.0);
-                } else {
-                    assertEquals(true, false, "No match found");
-                }
-            }
-
-            //Remove 2 (C) and replace it with 1 (A)
-            int deleteId = personController.getIdByName("C C");
-            personController.delValue(deleteId);
-            ticketController.distributeDebts(deleteId, personController.getIdByName("A A"));
-            System.out.println("Remove C and place A for it");
-
-            tallies = Calculator.CalculateFinalTallies(Calculator.CalculateTallyPairs());
-
-            Calculator.PrintTallies(tallies);
-
-            //It should be 1 -> 0 with 30.0
-            if ((personController.getNameById(tallies.get(0).getValue0()).equalsIgnoreCase("B") &&
-                    personController.getNameById(tallies.get(0).getValue1()).equalsIgnoreCase("A"))) {
-                assertEquals(tallies.get(0).getValue2(), 30.0);
-            } else {
-                assertEquals(true, false, "No match found");
-            }
-        }
-    }
-
-    @Test
-    public void testTallies() {
         //Create 3 persons
         List<IPerson> personList = createPersons(3);
 
@@ -380,56 +302,74 @@ public class IntegrationTest_Tallies {
         ITicketFactory ticketFactory = new DinnerTicketFactory();
         TicketController ticketController = new TicketController(TicketDB.getInstance());
 
-        //Create depth with dinner and uniform between 1 -> 0 for € 10.0
+        //Create depth with taxi and uniform between 0 -> 1 for € 20.0
         //0 (A) pays €20 for 1 (B)
         HashMap<Integer, Double> debts = new HashMap<>();
-        debts.put(personController.getIdByName(personList.get(0).toString()), 10.0);
-        ITicket ticket = ticketFactory.getUniformTicket(personController.getIdByName(personList.get(1).toString()), debts);
+        debts.put(personController.getIdByName(personList.get(1).toString()), 20.0);
+        ITicket ticket = ticketFactory.getUniformTicket(personController.getIdByName(personList.get(0).toString()), debts);
         ticketList.add(ticket);
 
-        //Create depth with dinner and uniform between 0 -> 1 for € 5.0
-        //1 (B) pays €10 for 0 (A)
+        //Create depth with dinner and uniform between 1 -> 0 for € 20.0
+        //1 (B) pays €20for 0 (A)
         debts = new HashMap<>();
-        debts.put(personController.getIdByName(personList.get(1).toString()), 5.0);
-        ticket = ticketFactory.getUniformTicket(personController.getIdByName(personList.get(0).toString()), debts);
-        ticketList.add(ticket);
+        debts.put(personController.getIdByName(personList.get(0).toString()), 20.0);
+        //ticket = ticketFactory.getVariableTicket(personController.getIdByName(personList.get(1).toString()), debts);
+        //ticketList.add(ticket);
 
-        //Create depth with dinner and uniform between 1 -> 2 for € 10.0
+        //Create depth with dinner and uniform between 1 -> 2 for € 30.0
         //1 (B) pays €30 for 2 (C)
-        debts = new HashMap<>();
-        //Het klopt nog niet helemaal
-        debts.put(personController.getIdByName(personList.get(2).toString()), 10.0);
-        ticket = ticketFactory.getUniformTicket(personController.getIdByName(personList.get(1).toString()), debts);
-        ticketList.add(ticket);
-
-
-        //Create depth with dinner and uniform between 0 -> 2 for € 10.0
-        //1 (B) pays €30 for 2 (C)
-        debts = new HashMap<>();
-        //Het klopt nog niet helemaal
-        debts.put(personController.getIdByName(personList.get(2).toString()), 10.0);
-        ticket = ticketFactory.getUniformTicket(personController.getIdByName(personList.get(0).toString()), debts);
+        //debts = new HashMap<>();
+        debts.put(personController.getIdByName(personList.get(2).toString()), 30.0);
+        ticket = ticketFactory.getVariableTicket(personController.getIdByName(personList.get(1).toString()), debts);
         ticketList.add(ticket);
 
         //save to the database
         ticketList.forEach(ticketController::addValue);
 
-
-        //Calculate the tallies. It should be 0 -> 1 with 10.0
+        //Calculate the tallies.
         List<Triplet<Integer, Integer, Double>> tallies = Calculator.CalculateTallyPairs();
-        Calculator.PrintTallies(tallies);
         tallies = Calculator.CalculateFinalTallies(tallies);
         Calculator.PrintTallies(tallies);
 
-        /*assertEquals(tallies.get(0).getValue0(), personController.getIdByName(personList.get(0).toString()));
-        assertEquals(tallies.get(0).getValue1(), personController.getIdByName(personList.get(1).toString()));
-        assertEquals(tallies.get(0).getValue2(), 10.0);
+        //It should be 0 -> 1 with 0
 
-        //AND 1 -> 2 with 30?
-        //TODO Tom kijk dit eens na
-        assertEquals(tallies.get(1).getValue0(), personController.getIdByName(personList.get(1).toString()));
-        assertEquals(tallies.get(1).getValue1(), personController.getIdByName(personList.get(2).toString()));
-        assertEquals(tallies.get(1).getValue2(), 30.0);*/
+        //It should be 2 -> 1 with 30.0
+        if (tallies.size() < 2)
+            if (personController.getNameById(tallies.get(0).getValue0()).equalsIgnoreCase("B") &&
+                    personController.getNameById(tallies.get(0).getValue1()).equalsIgnoreCase("C")) {
+                assertEquals(tallies.get(0).getValue2(), 30.0);
+            } else {
+                fail("No match found");
+            }
+        else {
+            if (personController.getNameById(tallies.get(0).getValue0()).equalsIgnoreCase("B") &&
+                    personController.getNameById(tallies.get(0).getValue1()).equalsIgnoreCase("C")) {
+                assertEquals(tallies.get(0).getValue2(), 30.0);
+            } else if (personController.getNameById(tallies.get(1).getValue0()).equalsIgnoreCase("B") &&
+                    personController.getNameById(tallies.get(1).getValue1()).equalsIgnoreCase("C")) {
+                assertEquals(tallies.get(1).getValue2(), 30.0);
+            } else {
+                fail("No match found");
+            }
+        }
+
+        //Remove 2 (C) and replace it with 1 (A)
+        int deleteId = personController.getIdByName("C C");
+        personController.delValue(deleteId);
+        ticketController.distributeDebts(deleteId, personController.getIdByName("A A"));
+        System.out.println("Remove C and place A for it");
+
+        tallies = Calculator.CalculateFinalTallies(Calculator.CalculateTallyPairs());
+
+        Calculator.PrintTallies(tallies);
+
+        //It should be 1 -> 0 with 30.0
+        if ((personController.getNameById(tallies.get(0).getValue0()).equalsIgnoreCase("B") &&
+                personController.getNameById(tallies.get(0).getValue1()).equalsIgnoreCase("A"))) {
+            assertEquals(tallies.get(0).getValue2(), 30.0);
+        } else {
+            fail("No match found");
+        }
     }
 
     @Test
@@ -490,10 +430,21 @@ public class IntegrationTest_Tallies {
         tallies = Calculator.CalculateTallyPairs();
         tallies = Calculator.CalculateFinalTallies(tallies);
         Calculator.PrintTallies(tallies);
+
+        if ((personController.getNameById(tallies.get(0).getValue0()).equalsIgnoreCase("B") &&
+                personController.getNameById(tallies.get(0).getValue1()).equalsIgnoreCase("A"))) {
+            assertEquals(tallies.get(0).getValue2(), 30.0);
+        } else {
+            fail("No match found");
+        }
     }
 
     @Test
     public void testTalliesRemovalThanCalculate() {
+        //reset databases
+        TicketDB.getInstance().delAllValue();
+        PersonDB.getInstance().delAllValue();
+
         //reset databases
         TicketDB.getInstance().delAllValue();
         PersonDB.getInstance().delAllValue();
@@ -540,6 +491,13 @@ public class IntegrationTest_Tallies {
         List<Triplet<Integer, Integer, Double>> tallies = Calculator.CalculateTallyPairs();
         tallies = Calculator.CalculateFinalTallies(tallies);
         Calculator.PrintTallies(tallies);
+
+        if ((personController.getNameById(tallies.get(0).getValue0()).equalsIgnoreCase("B") &&
+                personController.getNameById(tallies.get(0).getValue1()).equalsIgnoreCase("A"))) {
+            assertEquals(tallies.get(0).getValue2(), 30.0);
+        } else {
+            fail("No match found");
+        }
     }
 
     private List<IPerson> createPersons(int amount) {
